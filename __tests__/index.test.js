c41ff3bde0ee705b3ad20737f75d8b78d3943896
Alpha <116849110+alpha-xek@users.noreply.github.com>
2022-11-21 03:34:32 +0530

    Test-cases fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,11 +4,6 @@ const param = ['./to-be-deleted-test-data'];
 
 describe('Delete Folder', () => {
     it('Should delete a given folder', () => {
-        let result = false;
-        if(fs.existsSync(param[0])) {
-            result = true;
-        }
-        expect(result).toBeDefined();
-        expect(result).toBeFalsy();
+        expect(fs.existsSync('./__tests__/to-be-deleted-test-data')).toBeFalsy();
     });
 });
