01b7d359a37814367e3baa84cc6272b217299e64
Alpha <116849110+alpha-xek@users.noreply.github.com>
2022-11-21 02:36:45 +0530

    Update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const param = ['./to-be-deleted-test-data'];
 
 describe('Delete Folder', () => {
-    it('should delete a given folder', () => {
+    it('Should delete a given folder', () => {
         let result = false;
         if(fs.existsSync(param[0])) {
             result = true;
